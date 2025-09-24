da6c884b4ac747ccecac7fd3147570d306390f2a
jalvarenga11 <juan.alvarenga@ess.hn>
2025-09-23 18:32:34 -0600

    manejo de clases pt 2

--- intro-dart/05-clases.dart
+++ intro-dart/05-clases.dart
@@ -3,12 +3,40 @@ import 'CuentaBancaria.dart';
 void main() {
   final cuenta1 = CuentaBancaria(titular: 'Juan Perez', numeroCuenta: 12345);
 
-  cuenta1.deposito(500);
-  cuenta1.deposito(300);
-  //TODO: manejar error
-  cuenta1.deposito(-300); // cuenta como "retiro"
+  try {
+    cuenta1.deposito(500);
+    cuenta1.deposito(300);
+    cuenta1.deposito(
+      -300,
+    ); // esta linea impide que se continue con la ejecucion
+  } catch (e) {
+    print(e);
+  }
+
+  cuenta1.retiro(200);
 
   print(cuenta1.saldo); // es una función (getter)
 
-  final cuenta2 = CuentaBancaria(titular: 'Maria Gomez', numeroCuenta: 67890);
+  final cuenta2 = CuentaBancaria.plazoFijo(
+    titular: 'Maria Gomez',
+    numeroCuenta: 67890,
+  );
+  final cuenta3 = CuentaBancaria.cuentaEmpresarial(
+    titular: 'Empresa XYZ',
+    numeroCuenta: 11223,
+    tipoCuenta: 'Cuenta Empresarial',
+  );
+
+  print(cuenta1.toString());
+  print(cuenta2.toString());
+  print(cuenta3.toString());
+
+  final datosCuenta = {
+    'titular': 'Carlos Lopez',
+    'numeroCuenta': 44556,
+    'tipoCuenta': 'Ahorros',
+  };
+
+  final cuenta4 = CuentaBancaria.fromJson(datosCuenta);
+  print(cuenta4.toString());
 }
--- intro-dart/CuentaBancaria.dart
+++ intro-dart/CuentaBancaria.dart
@@ -1,21 +1,66 @@
-import 'dart:async';
-
 class CuentaBancaria {
   // propieades
 
   final String titular;
   final int numeroCuenta;
+  final String tipoCuenta;
 
   // propiedad privada
   double _saldo = 0;
 
   // Firma (constructor)
-  CuentaBancaria({required this.titular, required this.numeroCuenta});
+  CuentaBancaria({
+    required this.titular,
+    required this.numeroCuenta,
+    this.tipoCuenta = 'Ahorros',
+  });
+
+  // constructor con nombre
+  CuentaBancaria.plazoFijo({
+    required this.titular,
+    required this.numeroCuenta,
+    this.tipoCuenta = 'Plazo Fijo',
+  });
+
+  CuentaBancaria.cuentaEmpresarial({
+    required this.titular,
+    required this.numeroCuenta,
+    required this.tipoCuenta,
+  });
+
+  // constructor factory
+  factory CuentaBancaria.fromJson(Map<String, dynamic> datos) {
+    return CuentaBancaria(
+      titular: datos['titular'],
+      numeroCuenta: datos['numeroCuenta'],
+      tipoCuenta: datos['tipoCuenta'],
+    );
+  }
 
   // comportamientos / funciones o metodos
 
   double deposito(double monto) {
+    if (monto <= 0) {
+      // generar una excepcion
+      throw Exception('El monto del deposito debe ser positivo');
+    }
+
     saldo += monto;
+    return saldo;
+  }
+
+  double retiro(double monto) {
+    if (monto <= 0) {
+      // generar una excepcion
+      throw Exception('El monto del retiro debe ser positivo');
+    }
+
+    if (monto > saldo) {
+      // generar una excepcion
+      throw Exception('No hay saldo suficiente');
+    }
+
+    saldo -= monto;
     return saldo;
   }
 
@@ -39,4 +84,9 @@ class CuentaBancaria {
   // void setSaldo(double saldo) {
   //   this._saldo = saldo;
   // }
+
+  @override
+  String toString() {
+    return 'Titula: $titular, Numero de Cuenta: $numeroCuenta, Tipo de Cuenta: $tipoCuenta';
+  }
 }
