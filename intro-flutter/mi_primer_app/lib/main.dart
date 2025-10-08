b6622069cc0a86d9bb9916ccdb5fe05e8e28c9d1
jalvarenga11 <juan.alvarenga@ess.hn>
2025-10-07 18:54:14 -0600

    manejo de estados y diseño con Columns

--- intro-flutter/mi_primer_app/lib/main.dart
+++ intro-flutter/mi_primer_app/lib/main.dart
@@ -8,17 +8,78 @@ void main() {
 // StatelessWidget -> Son Widgets que no tienen estado mutable
 // StatefulWidget -> Son Widgets que tienen estado mutable
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int contador = 0;
+
   @override
   Widget build(BuildContext context) {
+    print('actualizacion del widget (estado)');
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.purple),
-        body: Center(child: Text('Hola mundo')),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Hola mundo');
-          },
+        appBar: AppBar(
+          centerTitle: false,
+          backgroundColor: Colors.teal[700],
+          title: Text(
+            'Mi Primer App',
+            style: TextStyle(color: Colors.teal[50]),
+          ),
+        ),
+        // appBar: AppBar(backgroundColor: Color(0xFF242424)),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Hola mundo',
+                style: TextStyle(fontSize: 50, color: Colors.black87),
+              ),
+              Text(
+                '$contador',
+                style: TextStyle(fontSize: 45, color: Colors.black87),
+              ),
+            ],
+          ),
+        ),
+        // floatingActionButtonLocation:
+        //     FloatingActionButtonLocation.miniCenterFloat,
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+
+          children: [
+            FloatingActionButton(
+              backgroundColor: Colors.red[100],
+              child: Icon(
+                Icons.exposure_minus_1_outlined,
+                color: Colors.red[900],
+              ),
+              onPressed: () {
+                setState(() {});
+
+                contador--;
+                print(contador);
+              },
+            ),
+            SizedBox(height: 12),
+            FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                setState(() {});
+
+                contador++;
+                print(contador);
+              },
+            ),
+          ],
         ),
       ),
     ); // widget Padre
