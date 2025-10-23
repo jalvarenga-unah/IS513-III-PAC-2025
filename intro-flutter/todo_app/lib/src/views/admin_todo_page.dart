6deecfb9e59fdd4ac4294798caab62d7e3b2e08d
jalvarenga11 <juan.alvarenga@ess.hn>
2025-10-22 18:57:20 -0600

    pantalla para crear una tarea

--- intro-flutter/todo_app/lib/src/views/admin_todo_page.dart
+++ intro-flutter/todo_app/lib/src/views/admin_todo_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/api/todos.dart';
+import 'package:todo_app/src/shared/utils.dart';
 
 class AdminTodoPage extends StatelessWidget {
   AdminTodoPage({super.key});
 
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
+  FocusNode titleFocus = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +19,7 @@ class AdminTodoPage extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              focusNode: titleFocus,
               controller: titleController,
               decoration: InputDecoration(
                 label: Text('Titulo'),
@@ -29,6 +34,7 @@ class AdminTodoPage extends StatelessWidget {
                 // ),
                 prefixIcon: Icon(Icons.text_fields_rounded),
               ),
+
               maxLines: 1,
               maxLength: 50,
               obscureText: false,
@@ -36,14 +42,73 @@ class AdminTodoPage extends StatelessWidget {
               // style: TextStyle(color: Colors.red),
             ),
             SizedBox(height: 16),
-            TextField(controller: descriptionController),
+            TextField(
+              controller: descriptionController,
+              maxLines: 4,
+              decoration: InputDecoration(label: Text('Descripción')),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue[300],
         onPressed: () {
-          print(titleController.text); // obteniendo el valor del Title
+          //TODO: mostrar icono de carga usando gestores de estado
+
+          if (titleController.text.isEmpty) {
+            // ScaffoldMessenger.of(context).showSnackBar(
+            //   SnackBar(
+            //     content: Text(
+            //       'Debe ingresar un titulo',
+            //       style: TextStyle(color: Colors.red[50]),
+            //     ),
+            //     backgroundColor: Colors.red,
+            //   ),
+            // );
+
+            Utils.showSnackBar(
+              context: context,
+              title: "El titulo es obligatorio",
+              color: Colors.red,
+            );
+
+            return;
+          }
+
+          final Map<String, dynamic> newTodo = {
+            'id': todoList.length + 1,
+            'title': titleController.text,
+            'description': descriptionController.text,
+            'completed': false,
+          };
+          todoList.add(newTodo);
+
+          // final snackBar = SnackBar(
+          //   content: const Text('Yay! A SnackBar!'),
+          //   action: SnackBarAction(
+          //     label: 'Undo',
+          //     onPressed: () {
+          //       // Some code to undo the change.
+          //     },
+          //   ),
+          // );
+          // ScaffoldMessenger.of(context).showSnackBar(
+          //   SnackBar(
+          //     content: Text('Tarea creada correctamente'),
+          //     // backgroundColor: ,
+          //     duration: Duration(days: 4),
+          //     action: SnackBarAction(label: 'Cerrar', onPressed: () {}),
+          //   ),
+          // );
+          Utils.showSnackBar(
+            context: context,
+            title: "Tarea creada correctamente",
+          );
+
+          titleController.text = '';
+          descriptionController.text = '';
+
+          context.pop();
         },
         child: Icon(Icons.add, color: Colors.blue[50]),
       ),
